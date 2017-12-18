5abbc2d201f735d1c1a1c251adb62bf04eb0bc58
Matt Barlow <matt.barlow@rackspace.com>
2017-12-18 21:37:31 +0000

    Add keyvalout output
    Signed-off-by: Steve Katen <steve.katen@rackspace.com>

--- tasks/auth/task.sh
+++ tasks/auth/task.sh
@@ -2,11 +2,8 @@
 set -eu
 
 apk update
-apk add py-curl py-yaml curl-dev build-base jq
-export PYCURL_SSL_LIBRARY=openssl
-pip install pyresttest
+apk add jq
 
 export CF_AUTH_TOKEN=$(pyresttest https://$CF_API_URL cc-api-tests/tasks/auth/test.yml | jq -r .access_token)
 
-mkdir keyvalout
 echo "CF_AUTH_TOKEN=${CF_AUTH_TOKEN}" >> keyvalout/keyval.properties
