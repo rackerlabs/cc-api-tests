c720f99399bd07721d8c314785867a7acee413e3
Steve Katen <steve.katen@rackspace.com>
2017-12-18 21:59:22 +0000

    Add list all app usage events job
    Signed-off-by: BK Box <bk.box@rackspace.com>

--- tasks/auth/task.sh
+++ tasks/auth/task.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 set -eu
 
-apk update
-apk add jq
-
 export CF_AUTH_TOKEN=$(pyresttest https://$CF_API_URL cc-api-tests/tasks/auth/test.yml | jq -r .access_token)
 
+echo "CF_API_URL=${CF_API_URL}" >> keyvalout/keyval.properties
 echo "CF_AUTH_TOKEN=${CF_AUTH_TOKEN}" >> keyvalout/keyval.properties
---
+++ tasks/list_all_app_usage_events/task.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -eu
+
+props="${ROOT_FOLDER}/keyval/keyval.properties"
+if [ -f "$props" ]
+then
+  echo "Reading passed key values"
+  while IFS= read -r var
+  do
+    if [ ! -z "$var" ]
+    then
+      echo "Adding: $var"
+      export "$var"
+    fi
+  done < "$props"
+fi
+
+pyresttest https://$CF_API_URL cc-api-tests/tasks/list_all_app_usage_events/test.yml
