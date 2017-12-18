1926d3fc740410c5110653f97f4d482d4b4fa5cf
BK Box <bk.box@rackspace.com>
2017-12-18 20:58:28 +0000

    Add openssl workaround
    Signed-off-by: Matt Barlow <matt.barlow@rackspace.com>

--- tasks/auth/task.sh
+++ tasks/auth/task.sh
@@ -3,6 +3,7 @@ set -eu
 
 apk update
 apk add py-curl py-yaml curl-dev build-base
+export PYCURL_SSL_LIBRARY=openssl
 pip install pyresttest
 
 pyresttest https://$CF_API_URL cc-api-tests/tests/auth.yml
