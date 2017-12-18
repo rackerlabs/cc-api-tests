000ea36bf10c9b98728be1a9ad31fbb6acc534a8
Steve Katen <steve.katen@rackspace.com>
2017-12-18 21:18:45 +0000

    Use keyval resource
    Signed-off-by: BK Box <bk.box@rackspace.com>

--- tasks/auth/task.sh
+++ tasks/auth/task.sh
@@ -2,8 +2,10 @@
 set -eu
 
 apk update
-apk add py-curl py-yaml curl-dev build-base
+apk add py-curl py-yaml curl-dev build-base jq
 export PYCURL_SSL_LIBRARY=openssl
 pip install pyresttest
 
-pyresttest https://$CF_API_URL cc-api-tests/tasks/auth/test.yml
+export CF_AUTH_TOKEN=$(pyresttest https://$CF_API_URL cc-api-tests/tasks/auth/test.yml | jq -r .access_token)
+
+echo "CF_AUTH_TOKEN=${CF_AUTH_TOKEN}" >> keyval.properties
