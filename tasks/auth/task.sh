bca3f82960349852d952fb935ff22ace9a5d7fb2
Matt Barlow <matt.barlow@rackspace.com>
2017-12-18 21:00:57 +0000

    Correct path to test yml
    Signed-off-by: Steve Katen <steve.katen@rackspace.com>

--- tasks/auth/task.sh
+++ tasks/auth/task.sh
@@ -6,4 +6,4 @@ apk add py-curl py-yaml curl-dev build-base
 export PYCURL_SSL_LIBRARY=openssl
 pip install pyresttest
 
-pyresttest https://$CF_API_URL cc-api-tests/tests/auth.yml
+pyresttest https://$CF_API_URL cc-api-tests/tasks/auth/test.yml
