25403ecd2015d2cabab4a888923bdb3935fe64d3
BK Box <bk.box@rackspace.com>
2017-12-18 22:09:49 +0000

    Output response
    Signed-off-by: Matt Barlow <matt.barlow@rackspace.com>

--- tasks/auth/task.sh
+++ tasks/auth/task.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-export CF_AUTH_TOKEN=$(pyresttest https://$CF_API_URL cc-api-tests/tasks/auth/test.yml | jq -r .access_token)
+export CF_AUTH_TOKEN=$(pyresttest https://$CF_API_URL cc-api-tests/tasks/auth/test.yml --print-bodies| sed $d | jq -r .access_token)
 
 echo "CF_API_URL=${CF_API_URL}" >> keyvalout/keyval.properties
 echo "CF_AUTH_TOKEN=${CF_AUTH_TOKEN}" >> keyvalout/keyval.properties
