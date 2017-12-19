b3a839cb78da3259a138c3e164da9040f52fb2a2
mattjbarlow <matt.barlow@rackspace.com>
2017-12-19 16:54:36 +0000

    Fix params typo

--- tasks/auth/task.sh
+++ tasks/auth/task.sh
@@ -3,9 +3,6 @@ set -eu
 
 export CF_AUTH_TOKEN=$(pyresttest https://$CF_API_URL cc-api-tests/tasks/auth/test.yml --print-bodies=true | sed '$d' | jq -r .access_token)
 
-echo "CF_API_URL=${CF_API_URL}" >> cc-api-tests-bucket/app-usage-events.txt
-echo "CF_AUTH_TOKEN=${CF_AUTH_TOKEN}" >> cc-api-tests-bucket/app-usage-events.txt
-
 props="cc-api-tests-put-bucket/app-usage-events.txt"
 echo "Setting key values for next job"
 while IFS='=' read -r name value ; do
