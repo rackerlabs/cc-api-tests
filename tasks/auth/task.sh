69ab5f5acc50c296e17e68f97ed73b3ed2bdf4c0
mattjbarlow <matt.barlow@rackspace.com>
2017-12-19 18:00:10 +0000

    Move read and write into functions

--- tasks/auth/task.sh
+++ tasks/auth/task.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 set -eu
 
+source cc-api-tests/functions/functions.sh
+
 export CF_AUTH_TOKEN=$(pyresttest https://$CF_API_URL cc-api-tests/tasks/auth/test.yml --print-bodies=true | sed '$d' | jq -r .access_token)
 
-props="cc-api-tests-put-bucket/app-usage-events.txt"
-echo "Setting key values for next job"
-while IFS='=' read -r name value ; do
-    if [[ $name == 'CF_'* ]]; then
-	echo "Adding: ${name}"
-    fi
-done < <(env)
+write_env_vars_to_file
--- tasks/list_all_app_usage_events/task.sh
+++ tasks/list_all_app_usage_events/task.sh
@@ -1,25 +1,10 @@
 #!/bin/sh
 set -eu
 
-props="cc-api-tests-bucket/app-usage-events.txt"
-if [ -f "$props" ]
-then
-  echo "Reading passed key values"
-  while IFS= read -r var
-  do
-    if [ ! -z "$var" ]
-    then
-      export "$var"
-    fi
-  done < "$props"
-fi
+source cc-api-tests/functions/functions.sh
+
+read_env_vars_from_file
 
 export CF_APP_USAGE_EVENT_GUID=$(pyresttest --print-bodies=true https://$CF_API_URL cc-api-tests/tasks/list_all_app_usage_events/test.yml | sed '$d' | jq -r .resources[0].metadata.guid)
 
-props="cc-api-tests-put-bucket/app-usage-events.txt"
-echo "Setting key values for next job"
-while IFS='=' read -r name value ; do
-    if [[ $name == 'CF_'* ]]; then
-	echo "Adding: ${name}"
-    fi
-done < <(env)
+write_env_vars_to_file
