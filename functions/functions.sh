04d1949e4e24b0298b512eec053796cad5aa3c0a
mattjbarlow <matt.barlow@rackspace.com>
2017-12-19 18:00:25 +0000

    Create functions file

---
+++ functions/functions.sh
@@ -0,0 +1,26 @@
+function write_env_vars_to_file () (
+    set -eu
+    props="cc-api-tests-put-bucket/app-usage-events.txt"
+    echo "Setting key values for next job"
+    while IFS='=' read -r name value ; do
+	if [[ $name == 'CF_'* ]]; then
+	    echo "Adding: ${name}"
+	fi
+    done < <(env)
+    )
+
+function read_env_vars_from_file () (
+    set -eu
+    props="cc-api-tests-bucket/app-usage-events.txt"
+    if [ -f "$props" ]
+    then
+	echo "Reading passed key values"
+	while IFS= read -r var
+	do
+	    if [ ! -z "$var" ]
+	    then
+		export "$var"
+	    fi
+	done < "$props"
+    fi
+)
