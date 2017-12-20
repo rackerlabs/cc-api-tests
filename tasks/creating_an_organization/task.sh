a140aaff484b09ed16d9073884fecd9687b6a88a
mattjbarlow <matt.barlow@rackspace.com>
2017-12-20 21:20:10 +0000

    Fix pyresttest command

--- tasks/creating_an_organization/task.sh
+++ tasks/creating_an_organization/task.sh
@@ -4,7 +4,7 @@ set -eu
 source cc-api-tests/util/functions.sh
 source cc-api-tests/util/read_env_vars_from_file.sh
 
-pyresttest --print-bodies=true https://$CF_API_URL cc-api-tests/tasks/creating_an_organization/test.yml
-export CF_ORGANIZATION_GUID=$(pyresttest https://$CF_API_URL cc-api-tests/tasks/auth/test.yml --print-bodies=true | sed '$d' | jq -r .metadata.guid)
+
+export CF_ORGANIZATION_GUID=$(pyresttest --print-bodies=true https://$CF_API_URL cc-api-tests/tasks/creating_an_organization/test.yml | sed '$d' | jq -r .metadata.guid)
 
 write_env_vars_to_file
