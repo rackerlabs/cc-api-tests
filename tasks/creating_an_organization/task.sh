c7b2b0fe78f3792d782a6c5e1edd7bb8732505f6
mattjbarlow <matt.barlow@rackspace.com>
2017-12-20 21:17:14 +0000

    Add delete org test

--- tasks/creating_an_organization/task.sh
+++ tasks/creating_an_organization/task.sh
@@ -5,3 +5,6 @@ source cc-api-tests/util/functions.sh
 source cc-api-tests/util/read_env_vars_from_file.sh
 
 pyresttest --print-bodies=true https://$CF_API_URL cc-api-tests/tasks/creating_an_organization/test.yml
+export CF_ORGANIZATION_GUID=$(pyresttest https://$CF_API_URL cc-api-tests/tasks/auth/test.yml --print-bodies=true | sed '$d' | jq -r .metadata.guid)
+
+write_env_vars_to_file
---
+++ tasks/delete_a_particular_organization/task.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -eu
+
+source cc-api-tests/util/functions.sh
+source cc-api-tests/util/read_env_vars_from_file.sh
+
+pyresttest --print-bodies=true https://$CF_API_URL cc-api-tests/tasks/delete_a_particular_organization/test.yml
