2772dedbaafd8e4f4f7535bd3b9002fd7f7f5546
mattjbarlow <matt.barlow@rackspace.com>
2017-12-20 21:45:29 +0000

    Add delete space test

--- tasks/creating_a_space/task.sh
+++ tasks/creating_a_space/task.sh
@@ -4,4 +4,6 @@ set -eu
 source cc-api-tests/util/functions.sh
 source cc-api-tests/util/read_env_vars_from_file.sh
 
-pyresttest --print-bodies=true https://$CF_API_URL cc-api-tests/tasks/creating_a_space/test.yml
+export CF_SPACE_GUID=$(pyresttest --print-bodies=true https://$CF_API_URL cc-api-tests/tasks/creating_a_space/test.yml | sed '$d' | jq -r .metadata.guid)
+
+write_env_vars_to_file
---
+++ tasks/delete_a_particular_space/task.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eu
+
+source cc-api-tests/util/functions.sh
+source cc-api-tests/util/read_env_vars_from_file.sh
+
+pyresttest --print-bodies=true https://$CF_API_URL cc-api-tests/tasks/delete_a_particular_space/test.yml
+
+write_env_vars_to_file
