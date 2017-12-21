734ebea37ce8a308c1c52f595c8dd929a01e2c0c
mattjbarlow <matt.barlow@rackspace.com>
2017-12-21 16:02:16 +0000

    Fix call to run_test

--- tasks/creating_an_organization/task.sh
+++ tasks/creating_an_organization/task.sh
@@ -4,6 +4,6 @@ set -eu
 source cc-api-tests/util/functions.sh
 source cc-api-tests/util/read_env_vars_from_file.sh
 
-run_test creating_an_organization .metadata.guid
+run_test creating_an_organization CF_ORGANIZATION_GUID .metadata.guid
 
 write_env_vars_to_file
