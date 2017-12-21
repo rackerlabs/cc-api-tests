43c792987a8ca052e6960cabba985e34635d566a
mattjbarlow <matt.barlow@rackspace.com>
2017-12-21 15:27:40 +0000

    Update templates to always have outputs

--- templates/no-output-task.sh
+++ templates/no-output-task.sh
@@ -5,3 +5,5 @@ source cc-api-tests/util/functions.sh
 source cc-api-tests/util/read_env_vars_from_file.sh
 
 pyresttest --print-bodies=true https://$CF_API_URL cc-api-tests/tasks/TEST_NAME_WITH_UNDERSCORES/test.yml
+
+write_env_vars_to_file
