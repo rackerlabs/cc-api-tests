65eb7c46a552698136667e5725bb15df57b7acdb
mattjbarlow <matt.barlow@rackspace.com>
2017-12-21 20:05:53 +0000

    Add test for creating a shared domain

---
+++ tasks/create_a_shared_domain/task.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+source cc-api-tests/util/functions.sh
+source cc-api-tests/util/read_env_vars_from_file.sh
+
+run_test create_a_shared_domain CF_SHARED_DOMAIN_GUID .metadata.guid
+
+write_env_vars_to_file
