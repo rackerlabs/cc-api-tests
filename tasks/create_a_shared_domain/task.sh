bc9a8e1fe3c91dbec3231e5b54e3cbbfe600cd07
mattjbarlow <matt.barlow@rackspace.com>
2018-01-04 21:01:32 +0000

    Debug create a share domain

--- tasks/create_a_shared_domain/task.sh
+++ tasks/create_a_shared_domain/task.sh
@@ -4,6 +4,6 @@ set -ex
 source cc-api-tests/util/functions.sh
 source cc-api-tests/util/read_env_vars_from_file.sh
 
-run_test create_a_shared_domain CF_SHARED_DOMAIN_GUID .metadata.guid
-
+# run_test create_a_shared_domain CF_SHARED_DOMAIN_GUID .metadata.guid
+pyresttest --print-bodies=true https://$CF_API_URL cc-api-tests/tasks/create_a_shared_domain/test.yml
 write_env_vars_to_file
