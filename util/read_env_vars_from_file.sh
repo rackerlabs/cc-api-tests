fbaae2d614aa96f56c1e92565e722013a6541ee4
mattjbarlow <matt.barlow@rackspace.com>
2018-01-04 17:25:07 +0000

    Read variables from all files

--- util/read_env_vars_from_file.sh
+++ util/read_env_vars_from_file.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-props="${INPUT}/${PIPELINE}.txt"
-if [ -f "$props" ]
-then
-    while IFS= read -r var
-    do
-	if [ ! -z "$var" ]
-	then
-	    export $var
-	fi
-    done < "$props"
-fi
+props=$(find . -name ${PIPELINE}.txt -exec cat {} \;)
+while IFS= read -r var
+do
+    if [ ! -z "$var" ]
+    then
+	export $var
+    fi
+done <<< "$props"
