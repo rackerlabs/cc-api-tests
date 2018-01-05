19bd3135085eb553629b6c636bc64b4dccb09e33
BK Box <bk.box@rackspace.com>
2018-01-05 20:21:22 +0000

    Add required host key
    Signed-off-by: Steve Katen <steve.katen@rackspace.com>

--- tasks/creating_a_route/task.sh
+++ tasks/creating_a_route/task.sh
@@ -5,6 +5,7 @@ source cc-api-tests/util/functions.sh
 source cc-api-tests/util/read_env_vars_from_file.sh
 
 PYOUTPUT=$(pyresttest https://$CF_API_URL cc-api-tests/tasks/creating_a_route/test.yml --print-bodies=true)
+echo "${PYOUTPUT}"
 CF_ROUTE_GUID=$(echo "${PYOUTPUT}" | sed '$d' | jq -r .metadata.guid)
 export CF_ROUTE_GUID
 
