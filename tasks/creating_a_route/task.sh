4c2164ee0ddc4a0696e517c9072c0abd1e2c9e8c
Steve Katen <steve.katen@rackspace.com>
2018-01-05 20:10:33 +0000

    fix formatting of yml file
    Signed-off-by: BK Box <bk.box@rackspace.com>

--- tasks/creating_a_route/task.sh
+++ tasks/creating_a_route/task.sh
@@ -5,7 +5,6 @@ source cc-api-tests/util/functions.sh
 source cc-api-tests/util/read_env_vars_from_file.sh
 
 PYOUTPUT=$(pyresttest https://$CF_API_URL cc-api-tests/tasks/creating_a_route/test.yml --print-bodies=true)
-
 CF_ROUTE_GUID=$(echo "${PYOUTPUT}" | sed '$d' | jq -r .metadata.guid)
 export CF_ROUTE_GUID
 
