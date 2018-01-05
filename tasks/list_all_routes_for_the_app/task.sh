113dd0040f210a0fe8dc8ed2d4ef9c8294ab319e
Matt Barlow <matt.barlow@rackspace.com>
2018-01-05 16:36:44 +0000

    Add list all routes for the app
    Signed-off-by: BK Box <bk.box@rackspace.com>

--- tasks/list_all_routes_for_the_app/task.sh
+++ tasks/list_all_routes_for_the_app/task.sh
@@ -4,6 +4,4 @@ set -e
 source cc-api-tests/util/functions.sh
 source cc-api-tests/util/read_env_vars_from_file.sh
 
-run_test list_all_routes_for_the_app  
-
-write_env_vars_to_file
+pyresttest --print-bodies=true https://$CF_API_URL cc-api-tests/tasks/list_all_routes_for_the_app/test.yml
