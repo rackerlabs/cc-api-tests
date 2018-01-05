d70e1fb48ef42368ef25ed51895cb5d7ded30246
mattjbarlow <matt.barlow@rackspace.com>
2018-01-05 19:59:39 +0000

    Add interim variable to catch errors

--- tasks/creating_a_route/task.sh
+++ tasks/creating_a_route/task.sh
@@ -4,7 +4,9 @@ set -e
 source cc-api-tests/util/functions.sh
 source cc-api-tests/util/read_env_vars_from_file.sh
 
-CF_ROUTE_GUID=$(pyresttest https://$CF_API_URL cc-api-tests/tasks/creating_a_route/test.yml --print-bodies=true | sed '$d' | jq -r .metadata.guid)
+PYOUTPUT=$(pyresttest https://$CF_API_URL cc-api-tests/tasks/creating_a_route/test.yml --print-bodies=true)
+
+CF_ROUTE_GUID=$(echo "${PYOUTPUT}" | sed '$d' | jq -r .metadata.guid)
 export CF_ROUTE_GUID
 
 write_env_vars_to_file
