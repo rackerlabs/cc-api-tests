7adefd49f66c335259995b99d65c120713b2ddc8
BK Box <bk.box@rackspace.com>
2018-01-05 19:48:35 +0000

    Write route guid to file
    Signed-off-by: Steve Katen <steve.katen@rackspace.com>

--- tasks/creating_a_route/task.sh
+++ tasks/creating_a_route/task.sh
@@ -6,3 +6,5 @@ source cc-api-tests/util/read_env_vars_from_file.sh
 
 CF_ROUTE_GUID=$(pyresttest https://$CF_API_URL cc-api-tests/tasks/creating_a_route/test.yml --print-bodies=true | sed '$d' | jq -r .metadata.guid)
 export CF_ROUTE_GUID
+
+write_env_vars_to_file
