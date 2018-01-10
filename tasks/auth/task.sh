d7d4ced1a2c0229ce63003e5fdbebea0ca11d635
mattjbarlow <matt.barlow@rackspace.com>
2018-01-10 16:46:41 +0000

    Write auth token to output file

--- tasks/auth/task.sh
+++ tasks/auth/task.sh
@@ -6,4 +6,4 @@ source cc-api-tests/util/functions.sh
 AUTH_TOKEN=$(pyresttest https://$CF_API_URL cc-api-tests/tasks/auth/test.yml --print-bodies=true | sed '$d' | jq -r .access_token)
 export AUTH_TOKEN
 
-write_env_vars_to_file
+echo "AUTH_TOKEN=${AUTH_TOKEN}" >> "${OUTPUT}/${PIPELINE}.txt"
