aebffa66621e0f934a837b3f43e19c2a1fa274a1
mattjbarlow <matt.barlow@rackspace.com>
2018-01-10 22:25:14 +0000

    Test failure mode

--- tasks/create_a_shared_domain/task.sh
+++ tasks/create_a_shared_domain/task.sh
@@ -4,6 +4,8 @@ set -eu
 source cc-api-tests/util/functions.sh
 source cc-api-tests/util/read_env_vars_from_file.sh
 
+exit 1
+
 CF_SHARED_DOMAIN_GUID=$(pyresttest https://$CF_API_URL cc-api-tests/tasks/create_a_shared_domain/test.yml --print-bodies=true | sed '$d' | jq -r .metadata.guid)
 export CF_SHARED_DOMAIN_GUID
 write_env_vars_to_file
