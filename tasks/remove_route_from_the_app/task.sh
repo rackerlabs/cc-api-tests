03b2c9ad9e78733b9625e8606c1af20c8e4e10e4
mattjbarlow <matt.barlow@rackspace.com>
2018-01-10 15:57:50 +0000

    Remove route from an app

---
+++ tasks/remove_route_from_the_app/task.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -eu
+
+source cc-api-tests/util/functions.sh
+source cc-api-tests/util/read_env_vars_from_file.sh
+
+pyresttest https://$CF_API_URL cc-api-tests/tasks/remove_route_from_the_app/test.yml --print-bodies=true
