2b9d193d5cffebd8aa1fad33a9d153ae6e71392e
mattjbarlow <matt.barlow@rackspace.com>
2018-01-10 16:51:07 +0000

    Continue to write env vars to file

--- tasks/auth/task.sh
+++ tasks/auth/task.sh
@@ -5,5 +5,6 @@ source cc-api-tests/util/functions.sh
 
 AUTH_TOKEN=$(pyresttest https://$CF_API_URL cc-api-tests/tasks/auth/test.yml --print-bodies=true | sed '$d' | jq -r .access_token)
 export AUTH_TOKEN
-
 echo "AUTH_TOKEN=${AUTH_TOKEN}" >> "${OUTPUT}/${PIPELINE}.txt"
+
+write_env_vars_to_file
