c2213bc3be85b8c7c579b99430c2921112f966c5
mattjbarlow <matt.barlow@rackspace.com>
2018-01-10 22:21:24 +0000

    Add on-failure handler

---
+++ tasks/on_failure/task.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -eu
+
+if [ -z "${DATADOG_API_KEY}" ]; then
+	exit
+else
+    post_to_datadog 0
+    curl  -X POST -H "Content-type: application/json" \
+	  -d '{
+      "title": "API Test Failure for ${FOUNDATION}.${PIPELINE}.${TASK}",
+      "text": "Reported.",
+      "priority": "normal",
+      "tags": ["application:cc-api-tests"],
+      "alert_type": "error"
+  }' \
+	  "https://app.datadoghq.com/api/v1/events?api_key=${DATADOG_API_KEY}"
+fi
