21306a6470776f876040f40ad25f18c0287eb5cf
mattjbarlow <matt.barlow@rackspace.com>
2018-01-10 16:38:18 +0000

    Do not save auth token to s3

--- tasks/auth/task.sh
+++ tasks/auth/task.sh
@@ -3,7 +3,7 @@ set -eu
 
 source cc-api-tests/util/functions.sh
 
-CF_AUTH_TOKEN=$(pyresttest https://$CF_API_URL cc-api-tests/tasks/auth/test.yml --print-bodies=true | sed '$d' | jq -r .access_token)
-export CF_AUTH_TOKEN
+AUTH_TOKEN=$(pyresttest https://$CF_API_URL cc-api-tests/tasks/auth/test.yml --print-bodies=true | sed '$d' | jq -r .access_token)
+export AUTH_TOKEN
 
 write_env_vars_to_file
