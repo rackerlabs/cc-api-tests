36a86922b773eebfce8c6f8b349369392eff730e
mattjbarlow <matt.barlow@rackspace.com>
2018-01-15 17:34:37 +0000

    Fix directories for pyresttest

--- tasks/creating_a_source_app/task.sh
+++ tasks/creating_a_source_app/task.sh
@@ -4,7 +4,7 @@ set -eu
 source cc-api-tests/util/functions.sh
 source cc-api-tests/util/read_env_vars_from_file.sh
 
-CF_SOURCE_APP_GUID=$(pyresttest https://$CF_API_URL cc-api-tests/tasks/creating_an_app/test.yml --print-bodies=true | sed '$d' | jq -r .metadata.guid)
+CF_SOURCE_APP_GUID=$(pyresttest https://$CF_API_URL cc-api-tests/tasks/creating_a_source_app/test.yml --print-bodies=true | sed '$d' | jq -r .metadata.guid)
 export CF_SOURCE_APP_GUID
 
 write_env_vars_to_file
--- tasks/delete_a_particular_source_app/task.sh
+++ tasks/delete_a_particular_source_app/task.sh
@@ -4,6 +4,6 @@ set -e
 source cc-api-tests/util/functions.sh
 source cc-api-tests/util/read_env_vars_from_file.sh
 
-pyresttest --print-bodies=true https://$CF_API_URL cc-api-tests/tasks/delete_a_particular_app/test.yml
+pyresttest --print-bodies=true https://$CF_API_URL cc-api-tests/tasks/delete_a_particular_source_app/test.yml
 
 post_to_datadog 1
