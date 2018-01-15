5aea278e29b14b77ac4f66fa487759b6560d118d
mattjbarlow <matt.barlow@rackspace.com>
2018-01-15 19:45:37 +0000

    Add copy bits test

---
+++ tasks/copy_the_app_bits_for_an_app/task.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eu
+
+source cc-api-tests/util/functions.sh
+source cc-api-tests/util/read_env_vars_from_file.sh
+
+pyresttest https://$CF_API_URL cc-api-tests/tasks/copy_the_app_bits_for_an_app/test.yml --print-bodies=true
+
+post_to_datadog 1
