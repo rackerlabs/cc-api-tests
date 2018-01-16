6b668da1ed225244cac681461a9ee8efabf3d22f
mattjbarlow <matt.barlow@rackspace.com>
2018-01-16 20:54:59 +0000

    Add cleanup task

---
+++ tasks/cleanup/task.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -eu
+
+source cc-api-tests/util/read_env_vars_from_file.sh
+
+curl -k -H "Authorization: bearer $AUTH_TOKEN" -X DELETE "https://$CF_API_URL/v2/organizations/$CF_ORG_GUID?recursive=true"
