9d2e183ee0c4e0cb109a396d868c1a3551919fb3
mattjbarlow <matt.barlow@rackspace.com>
2018-01-16 17:49:46 +0000

    Add instance information

--- tasks/get_the_instance_information_for_a_started_app/task.sh
+++ tasks/get_the_instance_information_for_a_started_app/task.sh
@@ -4,6 +4,6 @@ set -e
 source cc-api-tests/util/functions.sh
 source cc-api-tests/util/read_env_vars_from_file.sh
 
-pyresttest --print-bodies=true https://$CF_API_URL cc-api-tests/tasks/get_the_env_for_an_app/test.yml
+pyresttest --print-bodies=true https://$CF_API_URL cc-api-tests/tasks/get_the_instance_information_for_a_started_app/test.yml
 
 post_to_datadog 1
