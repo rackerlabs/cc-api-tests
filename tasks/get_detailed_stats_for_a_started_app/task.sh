e6003dc6d6649cdb476c533a85efddf21b10a77c
mattjbarlow <matt.barlow@rackspace.com>
2018-01-16 17:40:25 +0000

    Get detailed stats

---
+++ tasks/get_detailed_stats_for_a_started_app/task.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+source cc-api-tests/util/functions.sh
+source cc-api-tests/util/read_env_vars_from_file.sh
+
+pyresttest --print-bodies=true https://$CF_API_URL cc-api-tests/tasks/get_detailed_stats_for_a_started_app/test.yml
+
+post_to_datadog 1
