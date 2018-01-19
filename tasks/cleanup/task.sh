0715ee96b10cca35d6716b14b384228ca207139e
mattjbarlow <matt.barlow@rackspace.com>
2018-01-19 20:21:33 +0000

    Fix variable name for cleanup

--- tasks/cleanup/task.sh
+++ tasks/cleanup/task.sh
@@ -3,4 +3,4 @@ set -eu
 
 source cc-api-tests/util/read_env_vars_from_file.sh
 
-curl -k -H "Authorization: bearer $AUTH_TOKEN" -X DELETE "https://$CF_API_URL/v2/organizations/$CF_ORG_GUID?recursive=true"
+curl -k -H "Authorization: bearer $AUTH_TOKEN" -X DELETE "https://$CF_API_URL/v2/organizations/$CF_ORGANIZATION_GUID?recursive=true"
