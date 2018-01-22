87ef9218afb1f66cec9c2d0f30e00596cb4ee454
mattjbarlow <matt.barlow@rackspace.com>
2018-01-22 21:38:42 +0000

    Fix command format

--- tasks/bootstrap_cc_api_tests/task.sh
+++ tasks/bootstrap_cc_api_tests/task.sh
@@ -5,7 +5,7 @@ uaac target "${CF_UAA_URI}"
 uaac token client get admin -s "${CF_UAA_ADMIN_CLIENT_SECRET}"
 
 set +e
-uaac get user cc-api-tests
+uaac user get cc-api-tests
 set -e
 
 if [ $? -eq 0 ]
