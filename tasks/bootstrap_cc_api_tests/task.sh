0a08f721029dfe426be68871fda8e607fb384a2b
mattjbarlow <matt.barlow@rackspace.com>
2018-01-22 21:59:55 +0000

    Make bootstrap idempotent

--- tasks/bootstrap_cc_api_tests/task.sh
+++ tasks/bootstrap_cc_api_tests/task.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -eu
 
+add_uaac_member ()
+{
+    set +e
+    uaac user get cc-api-tests |grep "${1}"
+    if [ $? -ne 0 ]
+    then
+	uaac member "${1}" cc-api-tests
+    fi
+    set -e
+}
+
 uaac target "${CF_UAA_URI}"
 uaac token client get admin -s "${CF_UAA_ADMIN_CLIENT_SECRET}"
 
@@ -15,8 +26,8 @@ else
     uaac user add cc-api-tests -p "${CC_API_TEST_USER_PASSWORD}" --emails "${CC_API_TEST_USER_EMAIL}"
 fi
 
-uaac member add scim.read cc-api-tests
-uaac member add uaa.admin cc-api-tests
-uaac member add cloud_controller.admin cc-api-tests
-uaac member add scim.write cc-api-tests
-uaac member add routing.router_groups.read cc-api-tests
+add_uaac_member scim.read
+add_uaac_member uaa.admin
+add_uaac_member cloud_controller.admin
+add_uaac_member scim.write
+add_uaac_member routing.router_groups.read
