aa8fbf73675ae03907327039456099864bcd35ad
mattjbarlow <matt.barlow@rackspace.com>
2018-01-22 21:07:30 +0000

    Add bootstrap pipeline and task

---
+++ tasks/bootstrap_cc_api_tests/task.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -eu
+
+uaac target "${CF_UAA_URI}"
+uaac token client get admin -s "${CF_UAA_ADMIN_CLIENT_SECRET}"
+
+set +e
+uaac get user cc-api-tests
+set -e
+
+if [ $? -eq 0 ]
+then
+    echo "User already exists"
+else
+    uaac user add cc-api-tests -p "${CC_API_TEST_USER_PASSWORD}" --emails "${CC_API_TEST_USER_EMAIL}"
+fi
+
+uaac member add scim.read cc-api-tests
+uaac member add uaa.admin cc-api-tests
+uaac member add cloud_controller.admin cc-api-tests
+uaac member add scim.write cc-api-tests
+uaac member add routing.router_groups.read cc-api-tests
