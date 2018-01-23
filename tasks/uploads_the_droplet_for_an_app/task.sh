41d8c04063a030a055a241fc9404d190a21da574
Brian Lobert <brian.lobert@rackspace.com>
2018-01-23 21:45:55 +0000

    Fix uploads_the_droplet_for_an_app task script

--- tasks/uploads_the_droplet_for_an_app/task.sh
+++ tasks/uploads_the_droplet_for_an_app/task.sh
@@ -4,6 +4,6 @@ set -e
 source cc-api-tests/util/functions.sh
 source cc-api-tests/util/read_env_vars_from_file.sh
 
-curl -v -k -X PUT -H "Authorization: bearer $AUTH_TOKEN" -F "=droplet@cc-api-tests/fixtures/droplet/droplet.tgz" https://$CF_API_URL/v2/apps/$CF_APP_GUID/droplet/upload
+curl -v -k -X PUT -H "Authorization: bearer $AUTH_TOKEN" -F "droplet=@cc-api-tests/fixtures/droplet/droplet.tgz" https://$CF_API_URL/v2/apps/$CF_APP_GUID/droplet/upload
 
 post_to_datadog 1
