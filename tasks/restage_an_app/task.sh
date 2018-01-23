15684bfddc28e6614d5501c2d3d208fccf951833
Matt Barlow <matt.barlow@rackspace.com>
2018-01-23 19:12:38 +0000

    Add restage_app
    Signed-off-by: Brian Lobert <brian.lobert@rackspace.com>

---
+++ tasks/restage_an_app/task.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+source cc-api-tests/util/functions.sh
+source cc-api-tests/util/read_env_vars_from_file.sh
+
+pyresttest --print-bodies=true https://$CF_API_URL cc-api-tests/tasks/restage_an_app/test.yml
+
+post_to_datadog 1
