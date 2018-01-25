a537ba67161b2b1ba8f0133d0851d784c90f8251
Brian Lobert <brian.lobert@rackspace.com>
2018-01-25 00:17:49 +0000

    Fix variable name case

--- tasks/creating_a_user_provided_service_instance/task.sh
+++ tasks/creating_a_user_provided_service_instance/task.sh
@@ -4,8 +4,8 @@ set -e
 source cc-api-tests/util/functions.sh
 source cc-api-tests/util/read_env_vars_from_file.sh
 
-cf_user_provided_service_instance_guid=$(pyresttest --print-bodies=true https://$CF_API_URL cc-api-tests/tasks/creating_a_user_provided_service_instance/test.yml --print-bodies=true | sed '$d' | jq -r .metadata.guid)
-export cf_user_provided_service_instance_guid
+CF_USER_PROVIDED_SERVICE_INSTANCE_GUID=$(pyresttest --print-bodies=true https://$CF_API_URL cc-api-tests/tasks/creating_a_user_provided_service_instance/test.yml --print-bodies=true | sed '$d' | jq -r .metadata.guid)
+export CF_USER_PROVIDED_SERVICE_INSTANCE_GUID
 
 write_env_vars_to_file
 
