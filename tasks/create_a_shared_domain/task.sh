e0e81504a276ade6cb65c5b9c2b16ff008f1efb1
Brian Lobert <brian.lobert@rackspace.com>
2018-01-25 22:36:46 +0000

    Target an org before deleting shared domain

--- tasks/create_a_shared_domain/task.sh
+++ tasks/create_a_shared_domain/task.sh
@@ -9,8 +9,8 @@ CF_SHARED_DOMAIN_NAME=${PIPELINE}.com
 curl -L "https://packages.cloudfoundry.org/stable?release=linux64-binary&source=github" | tar -zx
 mv cf /usr/local/bin
 
-echo | cf login -u ${USERNAME} -p ${PASSWORD} -a ${CF_API_URL} --skip-ssl-validation
-cf delete-shared-domain ${CF_SHARED_DOMAIN_NAME}
+echo | cf login -u ${USERNAME} -p ${PASSWORD} -a ${CF_API_URL} -s system --skip-ssl-validation
+echo | cf delete-shared-domain ${CF_SHARED_DOMAIN_NAME}
 
 CF_SHARED_DOMAIN_GUID=$(pyresttest https://$CF_API_URL cc-api-tests/tasks/create_a_shared_domain/test.yml --print-bodies=true | sed '$d' | jq -r .metadata.guid)
 export CF_SHARED_DOMAIN_GUID
