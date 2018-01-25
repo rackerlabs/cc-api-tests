d6d15f0afca40c402cb6e13ff6512fa7f1445fb7
Brian Lobert <brian.lobert@rackspace.com>
2018-01-25 22:17:20 +0000

    Delete existing shared domain before creating

--- tasks/create_a_shared_domain/task.sh
+++ tasks/create_a_shared_domain/task.sh
@@ -4,6 +4,14 @@ set -eu
 source cc-api-tests/util/functions.sh
 source cc-api-tests/util/read_env_vars_from_file.sh
 
+CF_SHARED_DOMAIN_NAME=${PIPELINE}.com
+
+curl -L "https://packages.cloudfoundry.org/stable?release=linux64-binary&source=github" | tar -zx
+mv cf /usr/local/bin
+
+echo | cf login -u ${USERNAME} -p ${PASSWORD} -a ${CF_API_URL} --skip-ssl-validation
+cf delete-shared-domain ${CF_SHARED_DOMAIN_NAME}
+
 CF_SHARED_DOMAIN_GUID=$(pyresttest https://$CF_API_URL cc-api-tests/tasks/create_a_shared_domain/test.yml --print-bodies=true | sed '$d' | jq -r .metadata.guid)
 export CF_SHARED_DOMAIN_GUID
 write_env_vars_to_file
