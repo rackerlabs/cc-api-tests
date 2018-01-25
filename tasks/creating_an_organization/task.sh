908a5fb6ac8f3328fb4d5944f5bd81009c3d6d72
Brian Lobert <brian.lobert@rackspace.com>
2018-01-25 16:45:55 +0000

    Delete lingering test org

--- tasks/creating_an_organization/task.sh
+++ tasks/creating_an_organization/task.sh
@@ -4,6 +4,13 @@ set -eu
 source cc-api-tests/util/functions.sh
 source cc-api-tests/util/read_env_vars_from_file.sh
 
+CF_ORG_NAME=${PIPELINE}-ORG
+CF_ORGANIZATION_GUID=$(cf org ${CF_ORG_NAME} --guid)
+
+if [ !-z ${CF_ORGANIZATION_GUID} ]; then
+    curl -k -H "Authorization: bearer $AUTH_TOKEN" -X DELETE "https://$CF_API_URL/v2/organizations/$CF_ORGANIZATION_GUID?recursive=true"
+fi
+
 CF_ORGANIZATION_GUID=$(pyresttest https://$CF_API_URL cc-api-tests/tasks/creating_an_organization/test.yml --print-bodies=true | sed '$d' | jq -r .metadata.guid)
 export CF_ORGANIZATION_GUID
 
