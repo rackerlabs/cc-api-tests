fa4c65c1e92afa4a00e14a262b010504eb6dede4
Brian Lobert <brian.lobert@rackspace.com>
2018-01-26 22:37:30 +0000

    Decode urlencoded password

--- tasks/create_a_shared_domain/task.sh
+++ tasks/create_a_shared_domain/task.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eu
+set -exu
 
 source cc-api-tests/util/functions.sh
 source cc-api-tests/util/read_env_vars_from_file.sh
@@ -9,7 +9,9 @@ CF_SHARED_DOMAIN_NAME=${PIPELINE}.com
 curl -L "https://packages.cloudfoundry.org/stable?release=linux64-binary&source=github" | tar -zx
 mv cf /usr/local/bin
 
-echo | cf login -u ${USERNAME} -p ${PASSWORD} -a ${CF_API_URL} -o system --skip-ssl-validation
+DECODEDPW=$(python -c "import sys, urllib as ul; print ul.unquote_plus('${PASSWORD}')")
+
+echo | cf login -u ${USERNAME} -p ${DECODEDPW} -a ${CF_API_URL} -o system --skip-ssl-validation
 yes | cf delete-shared-domain ${CF_SHARED_DOMAIN_NAME}
 
 CF_SHARED_DOMAIN_GUID=$(pyresttest https://$CF_API_URL cc-api-tests/tasks/create_a_shared_domain/test.yml --print-bodies=true | sed '$d' | jq -r .metadata.guid)
